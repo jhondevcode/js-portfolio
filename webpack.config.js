29e6ed341150966c656835ea15261104ff0cc8b5
jhondevcode <jhondev.code@gmail.com>
2022-05-21 22:48:40 -0500

    Configuracion para procesado de css y stylus

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -19,13 +20,18 @@ module.exports = {
           loader: "babel-loader",
         },
       },
+      {
+        test: /\.css|.styl$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "stylus-loader"],
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       inject: true,
-      template: './public/index.html',
-      filename: 'index.html'
-    })
-  ]
+      template: "./public/index.html",
+      filename: "index.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
 };
